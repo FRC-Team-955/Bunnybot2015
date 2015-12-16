d21c77af844d702e6efbbd2794e99d9133d6ea91
Nosemaj-Yort <medked@gmail.com>
2015-12-15 17:52:17 -0800

    Finished auto and added coments

--- src/config/AutoConfig.java
+++ src/config/AutoConfig.java
@@ -2,10 +2,13 @@ package config;
 
 public class AutoConfig {
 	//TODO Add actual values
-	public static final double encDisPerPulse = 42;
 	
-	public static final double disDriveBackwards = 8;
+	public static final double disDriveBackwards = -8;
 	public static final double disDriveForward = 50;
+	public static final double maxEncTolerance = 2;
+	
+	public static final double driveForwardSpeed = 1;
+	public static final double driveBackwardSpeed = -.5;
 	
 	public static final double intakeTime = .5;
 }
--- src/core/Auto.java
+++ src/core/Auto.java
@@ -10,7 +10,7 @@ public class Auto {
 	Drive drive;
 	Timer tm = new Timer();
 	
-	private boolean isRunning = false;
+	private boolean isRunning = false;	//Whether the timer is running or not
 	private int counter = 0;
 	
 	public Auto(RobotCore robotCore, IntakeRamp ir1, IntakeRamp ir2, Drive drive){
@@ -20,12 +20,39 @@ public class Auto {
 		this.drive = drive;
 	}
 	
+	/**
+	 * Runs through autonomous actions
+	 */
 	public void run(){
 		switch(counter){
+			
+			case 0:{
+				driveBackwards();
+				break;
+			}
+			
+			case 1:{
+				runIntake2();
+				break;
+			}
+			
+			case 2:{
+				runIntake1();
+				break;
+			}
+			
+			case 3:{
+				driveForward();
+				break;
+			}
+			
 		
 		}
 	}
 	
+	/**
+	 * Runs front intake
+	 */
 	private void runIntake1(){
 		if(!isRunning)
 			tm.start();
@@ -41,6 +68,9 @@ public class Auto {
 		}
 	}
 	
+	/**
+	 * runs back intake
+	 */
 	private void runIntake2(){
 		if(!isRunning)
 			tm.start();
@@ -54,6 +84,38 @@ public class Auto {
 			ir2.stop();
 			counter++;
 		}
+	}
+	
+	/**
+	 * Drives forward until at a predetermined distance
+	 */
+	private void driveForward(){
+		//Checks if both sides are within an acceptable range of the target
+		if(Math.abs(robotcore.encRight.getDistance() - AutoConfig.disDriveForward) < AutoConfig.maxEncTolerance && Math.abs(robotcore.encLeft.getDistance() - AutoConfig.disDriveForward) < AutoConfig.maxEncTolerance){
+			drive.move(AutoConfig.disDriveForward, 0);
+		}
+		
+		else{
+			drive.move(0, 0);
+			counter++;
+		}
+			
+	}
+	
+	/**
+	 * Drives backwards until at a predetermined distance
+	 */
+	private void driveBackwards(){
+		//Checks if both sides are within an acceptable range of the target
+		if(Math.abs(robotcore.encRight.getDistance() - AutoConfig.disDriveBackwards) < AutoConfig.maxEncTolerance && Math.abs(robotcore.encLeft.getDistance() - AutoConfig.disDriveBackwards) < AutoConfig.maxEncTolerance){
+			drive.move(AutoConfig.disDriveBackwards, 0);
+		}
+		
+		else{
+			drive.move(0, 0);
+			counter++;
+		}
+			
 	}
 	
 }
